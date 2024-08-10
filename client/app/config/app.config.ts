f0b6a0b10e378e978797bd9e5812d598f09caa4a
justlogicalahmad <rabiu2993@gmail.com>
2024-08-10 11:47:40 -0700

    Fix: Config object type

--- client/app/config/app.config.ts
+++ client/app/config/app.config.ts
@@ -1,15 +1,8 @@
-interface ImportMetaEnv {
-  readonly VITE_DEV_SERVER_URL: string;
-  readonly VITE_PROD_SERVER_URL: string;
-  readonly VITE_NODE_ENV: string;
-  // Add other environment variables as needed
+interface Config {
+  server_base_url: string;
 }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-const config = {
+const config: Config = {
   server_base_url:
     import.meta.env.VITE_NODE_ENV !== "production"
       ? import.meta.env.VITE_DEV_SERVER_URL
