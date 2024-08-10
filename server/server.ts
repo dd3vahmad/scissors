1b12603487a6c316dee2f974971adad3cfb42a72
justlogicalahmad <rabiu2993@gmail.com>
2024-08-10 14:04:26 -0700

    Fix: Not binding network to cloud environment

--- server/server.ts
+++ server/server.ts
@@ -5,9 +5,7 @@ import app from "./app";
 
 const server = http.createServer(app);
 const PORT = Number(config.server.PORT);
-const HOSTNAME = config.server.HOSTNAME;
-const BACKLOG = Number(config.server.BACKLOG);
 
-server.listen(PORT, HOSTNAME, BACKLOG, () => {
+server.listen(PORT, "0.0.0.0", () => {
   logger.info(`Server running on port ${PORT}`);
 });
