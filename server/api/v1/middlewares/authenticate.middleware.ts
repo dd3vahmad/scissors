bb391ed81c36cb2172278b6cc4178fb30b6e88d0
justlogicalahmad <rabiu2993@gmail.com>
2024-08-11 13:25:45 -0700

    Feat: Authenticate with api key

--- server/api/v1/middlewares/authenticate.middleware.ts
+++ server/api/v1/middlewares/authenticate.middleware.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
 import config from "../../../config/server.config";
 import { error } from "../../../utils/error.util";
+import User from "../models/user.model";
+import { promisify } from "util";
 
 // Extend the Express Request interface to include the user property
 declare module "express-serve-static-core" {
@@ -10,25 +12,49 @@ declare module "express-serve-static-core" {
   }
 }
 
-const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.access_token;
+const authenticateToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies.access_token;
+    const authHeader = req.headers["authorization"];
 
-  if (!token) {
-    const res_error = error(401, "Token is required");
-    return res.status(401).json(res_error);
-  }
-
-  jwt.verify(
-    token,
-    config.server.app.auth.JWT_SECRET,
-    (err: VerifyErrors | null, user: string | JwtPayload | undefined) => {
-      if (err) {
-        return res.status(403).json({ message: "An error occurred" });
-      }
-      req.user = user;
-      next();
+    if (!token) {
+      const res_error = error(401, "Token is required");
+      return res.status(401).json(res_error);
     }
-  );
+
+    if (token) {
+      jwt.verify(
+        token,
+        config.server.app.auth.JWT_SECRET,
+        (err: VerifyErrors | null, user: string | JwtPayload | undefined) => {
+          if (err) {
+            return res.status(403).json({ message: "Not authenticated" });
+          }
+          req.user = user;
+        }
+      );
+    } else if (authHeader && authHeader.startsWith("Bearer ")) {
+      const apiKey = authHeader.split(" ")[1];
+
+      const user = await User.findOne({ apiKey: apiKey });
+
+      if (!user) {
+        const res_error = error(403, "Invalid API key");
+        return res.status(403).json(res_error);
+      }
+
+      req.user = user;
+    } else {
+      const res_error = error(403, "Token or API key required");
+      return res.status(403).json(res_error);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default authenticateToken;
